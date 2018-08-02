3feadfbfc936be3f265a87374ad1edec7b02df53
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-08-01 19:46:25 -0500

    committ

--- month assignment/Program.cs
+++ month assignment/Program.cs
@@ -6,7 +6,31 @@ namespace month_assignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("put in the number of a month");
+            int bob =
+            int.Parse(Console.ReadLine());
+
+            Console.WriteLine(bob);
+            if (bob == 2)
+            {
+                Console.WriteLine("February");
+            }
+
+            else if (bob == 3)
+            {
+                Console.WriteLine("March");
+            }
+            else if (bob == 4)
+            {
+                Console.WriteLine("April");
+            }
+            else
+            {
+                Console.WriteLine("hey dummy I wanted a 2");
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
